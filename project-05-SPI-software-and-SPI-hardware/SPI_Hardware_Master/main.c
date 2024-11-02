d213169cf9c37fb86fc821a06b04355d39c5a7fe
HoangTongXuan <107854635+HoangTongXuan@users.noreply.github.com>
2024-11-02 13:59:22 +0700

    Update SPI code

--- project-05-SPI-software-and-SPI-hardware/SPI_Hardware_Master/main.c
+++ project-05-SPI-software-and-SPI-hardware/SPI_Hardware_Master/main.c
@@ -1,23 +1,28 @@
 #include "stm32f10x.h"
 
+// Delay function in milliseconds
 void Delay_Ms(uint16_t Time)
 {
     TIM_SetCounter(TIM2, 0);
 
     while (TIM_GetCounter(TIM2) < Time * 10)
     {
-        // Delay timedelay*10*0,1ms = timedelay ms
+        // Delay is approximately Time ms, as Time * 10 * 0.1ms = Time ms
     }
 }
 
+// Timer configuration function
 void TIM_Config()
 {
+    // Enable the clock for TIM2
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
 
     TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
 
-    TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1; // 72MHz, because:72MHz/1=72MHz
+    // Set clock division to 1, resulting in a 72MHz clock frequency
+    TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
 
+    // Prescaler set to 7200 - 1, dividing the clock by 7200 (72MHz/7200 = 10kHz)
     TIM_TimeBaseInitStruct.TIM_Prescaler = 7200 - 1;
     TIM_TimeBaseInitStruct.TIM_Period = 0xFFFF;
     TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
@@ -25,69 +30,70 @@ void TIM_Config()
     TIM_Cmd(TIM2, ENABLE);
 }
 
-// Kh?i t?o SPI cho Master và c?u hình chân CS
+// Initialize SPI for Master mode and configure CS pin
 void SPI1_Master_Init(void)
 {
     SPI_InitTypeDef SPI_InitStructure;
     GPIO_InitTypeDef GPIO_InitStructure;
 
-    // B?t Clock cho SPI1 và GPIOA
+    // Enable the clock for SPI1 and GPIOA
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1 | RCC_APB2Periph_GPIOA, ENABLE);
 
-    // C?u hình chân GPIO cho SPI1: SCK, MISO, MOSI
+    // Configure GPIO pins for SPI1: SCK, MISO, MOSI
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-    // C?u hình chân CS (PA4) làm Output Push-Pull
+    // Configure CS pin (PA4) as Output Push-Pull
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
-    GPIO_SetBits(GPIOA, GPIO_Pin_4); // Ð?t CS ? m?c cao (không kích ho?t slave)
+    GPIO_SetBits(GPIOA, GPIO_Pin_4); // Set CS high (inactive, not selecting slave)
 
-    // C?u hình SPI ? ch? d? Master
+    // Configure SPI in Master mode
     SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
     SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
     SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
     SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
     SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
-    SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
+    SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;  // NSS managed by software
     SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_16;
     SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
     SPI_InitStructure.SPI_CRCPolynomial = 7;
+	
     SPI_Init(SPI1, &SPI_InitStructure);
 
-    // B?t SPI1
+    // Enable SPI1
     SPI_Cmd(SPI1, ENABLE);
 }
 
+// Transmit a single byte of data via SPI
 void SPI1_Master_Transmit(uint8_t data)
 {
-    GPIO_ResetBits(GPIOA, GPIO_Pin_4); // Kéo CS xu?ng m?c th?p d? kích ho?t slave
+    GPIO_ResetBits(GPIOA, GPIO_Pin_4); // Pull CS low to activate slave
 
-    while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET)
-        ;                         // Ch? TXE du?c set
-    SPI_I2S_SendData(SPI1, data); // G?i d? li?u
-    while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) == RESET)
-        ;                      // Ch? d? li?u nh?n
-    SPI_I2S_ReceiveData(SPI1); // Ð?c d? li?u nh?n (b? qua)
+    while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);		// Wait for TXE (transmit buffer empty) to be set
+    SPI_I2S_SendData(SPI1, data); // Send data
+    while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) == RESET);	// Wait until data is received
+    SPI_I2S_ReceiveData(SPI1); // Read received data (discarded)
 
-    GPIO_SetBits(GPIOA, GPIO_Pin_4); // Ð?t CS v? m?c cao sau khi truy?n xong
+    GPIO_SetBits(GPIOA, GPIO_Pin_4); // Set CS high after transmission is complete
 }
 
+// Data array to transmit
 uint8_t Data[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 int main(void)
 {
-    SPI1_Master_Init();
-    TIM_Config();
+    SPI1_Master_Init();  // Initialize SPI1 in Master mode
+    TIM_Config();        // Configure the timer
     while (1)
     {
         for (int i = 0; i < 10; i++)
         {
-            SPI1_Master_Transmit(Data[i]); // Truy?n d? li?u t? 0 d?n 9
-            Delay_Ms(500);
+            SPI1_Master_Transmit(Data[i]); // Transmit data from 0 to 9
+            Delay_Ms(500);                 // Delay for 500 ms
         }
     }
 }
--- project-05-SPI-software-and-SPI-hardware/SPI_Hardware_Slave/main.c
+++ project-05-SPI-software-and-SPI-hardware/SPI_Hardware_Slave/main.c
@@ -1,70 +1,73 @@
 #include "stm32f10x.h"
 
-// Kh?i t?o SPI cho Slave
+// Initialize SPI for Slave mode
 void SPI1_Slave_Init(void)
 {
     SPI_InitTypeDef SPI_InitStructure;
     GPIO_InitTypeDef GPIO_InitStructure;
 
-    // B?t Clock cho SPI1 và GPIOA
+    // Enable Clock for SPI1 and GPIOA
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1 | RCC_APB2Periph_GPIOA, ENABLE);
 
-    // C?u hình chân GPIO cho SPI1: SCK, MISO, MOSI
+    // Configure GPIO pins for SPI1: SCK, MISO, MOSI
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;  // Alternate Function Push-Pull
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 
+    // Configure GPIO pin for CS (Chip Select) as Floating Input
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING; // Ch? d? d?u vào v?i pull-up (n?u c?n)
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING; // Input floating mode for CS
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-    // C?u hình SPI ? ch? d? Slave
+    // Configure SPI in Slave mode
     SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
     SPI_InitStructure.SPI_Mode = SPI_Mode_Slave;
     SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
     SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
     SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
-    SPI_InitStructure.SPI_NSS = SPI_NSS_Hard; // Ch? d? NSS c?ng
+    SPI_InitStructure.SPI_NSS = SPI_NSS_Hard; // NSS is controlled by hardware
     SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
     SPI_InitStructure.SPI_CRCPolynomial = 7;
     SPI_Init(SPI1, &SPI_InitStructure);
 
-    // B?t SPI1
+    // Enable SPI1
     SPI_Cmd(SPI1, ENABLE);
 }
 
+// Function to receive data via SPI1 in Slave mode
 uint8_t SPI1_Slave_Receive(void)
 {
     while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) == RESET)
-        ;                             // Ch? nh?n d? li?u
-    return SPI_I2S_ReceiveData(SPI1); // Ð?c d? li?u nh?n du?c
+        ;                             // Wait until data is received
+    return SPI_I2S_ReceiveData(SPI1); // Read received data
 }
 
+// Alternative function to receive a single byte via SPI
 uint8_t SPI_Receive1Byte(void)
 {
     uint8_t temp;
-    while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_BSY) == 1)
-        ;
+    while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_BSY) == 1);		// Wait until SPI is not busy
     temp = (uint8_t)SPI_I2S_ReceiveData(SPI1);
-    while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) == 0)
-        ;
+    while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) == 0);	// Wait until data is received
     return temp;
 }
 
-uint8_t received_data = 0;
+uint8_t received_data = 0; // Variable to store received data
 
 int main(void)
 {
-    SPI1_Slave_Init();
+    SPI1_Slave_Init(); // Initialize SPI1 in Slave mode
 
     while (1)
     {
+        // Check if CS (PA4) is low, indicating data transfer
         if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_4) == 0)
         {
+            // Loop to receive data 10 times
             for (int i = 0; i < 10; i++)
             {
-                received_data = SPI1_Slave_Receive();
+                received_data = SPI1_Slave_Receive(); // Receive data and store in received_data
             }
         }
     }
